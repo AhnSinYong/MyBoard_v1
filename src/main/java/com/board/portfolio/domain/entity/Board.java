adde0c6ada2cc4426f0bfcc75076d3a334613b9c
전태준 <taejoon.jeon@gooroomee.com>
2019-11-20 17:44:43 +0900

    Board @Entity 생성

---
+++ src/main/java/com/board/portfolio/domain/entity/Board.java
@@ -0,0 +1,54 @@
+package com.board.portfolio.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Optional;
+
+@Entity
+@Table(name = "TB_BOARD")
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Board implements EntityDefaultValues{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="BID")
+    private Long bId;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "CONTENT")
+    private String content;
+
+    @Column(name="LIKE_")
+    private Integer like;
+
+    @Column(name="VIEW")
+    private Integer view;
+
+    @Column(name = "REG_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date regDate;
+
+    @Column(name = "UP_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date upDate;
+
+    @ManyToOne
+    @JoinColumn(name="EMAIL")
+    @JsonManagedReference
+    private Member member;
+
+
+    public void setDefaultValues() {
+        this.like = Optional.ofNullable(this.like).orElse(0);
+        this.view = Optional.ofNullable(this.view).orElse(0);
+        this.regDate = Optional.ofNullable(this.regDate).orElse(new Date());
+    }
+}
