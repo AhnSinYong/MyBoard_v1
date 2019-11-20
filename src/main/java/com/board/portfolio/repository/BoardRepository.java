139b75c3396b6e93e0e5a168fed7cba9e2691fba
전태준 <taejoon.jeon@gooroomee.com>
2019-11-20 17:45:39 +0900

    BoardRepository 생성

---
+++ src/main/java/com/board/portfolio/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.board.portfolio.repository;
+
+import com.board.portfolio.domain.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board,Long> {
+
+}
