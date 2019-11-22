2abdde86e85ac484dcc090f434f2c8103f1cb774
전태준 <taejoon.jeon@gooroomee.com>
2019-11-22 12:33:40 +0900

    MemberRepository 이름 변경 => AccountRepository

--- src/main/java/com/board/portfolio/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.board.portfolio.repository;
-
-import com.board.portfolio.domain.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,String> {
-
-}
