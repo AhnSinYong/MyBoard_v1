daa1175df7d4b36a7f25082e751ecdfe3d8c78e4
전태준 <taejoon.jeon@gooroomee.com>
2019-11-22 16:02:48 +0900

    Alarm @Entity 테스트 코드 작성

--- src/test/java/com/board/portfolio/DataBaseTests.java
+++ src/test/java/com/board/portfolio/DataBaseTests.java
@@ -2,7 +2,6 @@ package com.board.portfolio;
 
 import com.board.portfolio.domain.entity.*;
 import com.board.portfolio.repository.*;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,8 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.List;
-import java.util.UUID;
 
 @SpringBootTest
 class DataBaseTests {
@@ -26,7 +23,7 @@ class DataBaseTests {
     private String PW;
 
     @Autowired
-    MemberRepository memberRepository;
+    AccountRepository accountRepository;
     @Autowired
     BoardRepository boardRepository;
     @Autowired
@@ -37,6 +34,8 @@ class DataBaseTests {
     LikeCommentRepository likeCommentRepository;
     @Autowired
     FileAttachmentRepository fileAttachmentRepository;
+    @Autowired
+    AlarmRepository alarmRepository;
 
     @Test
     void dbConnectionTest() throws ClassNotFoundException {
@@ -50,128 +49,60 @@ class DataBaseTests {
             e.printStackTrace();
         }
     }
+//
+//    @Test
+//    @Transactional
+//    void saveMemberTableTest() {
+//        Account account = insertMember();
+//    }
+//    @Test
+//    @Transactional
+//    void saveBoardTableTest(){
+//        Board board = insertBoard();
+//    }
 
     @Test
     @Transactional
-    void saveMemberTableTest() {
-        Member member = insertMember();
-    }
-    @Test
-    @Transactional
-    void saveBoardTableTest(){
-        Board board = insertBoard();
-    }
-
-    @Test
-    @Transactional
-    void findMemberTest(){
-        Member member = findMember("test@naver.com");
-        List<Board> boardList = member.getBoardList();
-        Assertions.assertEquals("title",boardList.get(0).getTitle());
-
+    void findAccountTest(){
+        Account account = findMember("admin");
     }
     @Test
     @Transactional
     void findCommentTest(){
-        Comment comment_ = commentRepository.findById("test-for-custom-id").orElse(null);
+        Comment comment_ = commentRepository.findById("test-id").orElse(null);
 
     }
 
     @Test
     @Transactional
     void findLikeBoardTest(){
-        LikeBoard likeBoard = likeBoardRepository.findById("test-for-custom-id").orElse(null);
+        LikeBoard likeBoard = likeBoardRepository.findById("test-id").orElse(null);
     }
 
     @Test
     void findLikeCommentTest(){
 //        LikeComment comment = likeCommentRepository.save(buildLikeComment());
-        LikeComment commentLike = likeCommentRepository.findById("test-for-custom-id").orElse(null);
+        LikeComment commentLike = likeCommentRepository.findById("test-id").orElse(null);
     }
 
     @Test
     void findFileAtachmentTest(){
-        FileAttachment fileAttachment = fileAttachmentRepository.findById("test-for-custom-id").orElse(null);
+        FileAttachment fileAttachment = fileAttachmentRepository.findById("test-id").orElse(null);
+    }
+
+    @Test
+    void findAlarmTest(){
+        Alarm alarm = alarmRepository.findById("test-id").orElse(null);
     }
 
 
 
 
 
-    private Member findMember(String email){
-        return memberRepository.findById(email).orElse(null);
+    private Account findMember(String email){
+        return accountRepository.findById(email).orElse(null);
     }
 
-    private Member insertMember(){
-        Member member = buildMember();
-        return memberRepository.save(member);
-    }
-    private Member buildMember(){
-        return Member.builder()
-                .email("test@naver.com")
-                .password("1234")
-                .nickname("admin")
-                .socialId(UUID.randomUUID().toString())
-                .build();
-    }
-    private Board insertBoard(){
-        Board board = buildBoard();
-        return boardRepository.save(board);
-    }
-    private Board buildBoard(){
-        return Board.builder()
-                .title("title")
-                .content("content")
-                .member(buildMember()).build();
-    }
-    private Board buildBoard(Long bId){
-        return Board.builder()
-                .bId(bId)
-                .title("title")
-                .content("content")
-                .member(buildMember()).build();
-    }
-    private Comment insertComment(Long bId){
-        Comment comment = buildComment(bId);
-        return commentRepository.save(comment);
-    }
-    private Comment buildComment(){
-        return Comment.builder()
-                .cId("test-for-custom-id")
-                .board(Board.builder().bId((long)29).build())
-                .content("testtest")
-                .member(Member.builder().email("test@naver.com").build())
-                .build();
-    }
-    private Comment buildComment(Long bId){
-        return Comment.builder()
-                .cId("test-for-custom-id")
-                .board(Board.builder().bId((long)29).build())
-                .content("testtest")
-                .member(Member.builder().email("test@naver.com").build())
-                .build();
-    }
-
-    private LikeBoard insertLikeBoard(Long bId){
-        LikeBoard likeBoard = buildLikeBoard(bId);
-        return likeBoardRepository.save(likeBoard);
-    }
-
-    private LikeBoard buildLikeBoard(Long bId){
-        return LikeBoard.builder()
-                .id("test-for-custom-id")
-                .board(buildBoard(bId))
-                .member(buildMember())
-                .build();
-    }
-
-    private LikeComment buildLikeComment(){
-        return LikeComment.builder()
-                .id("test-for-custom-id")
-                .comment(buildComment())
-                .member(buildMember())
-                .build();
-    }
 
 
 }
