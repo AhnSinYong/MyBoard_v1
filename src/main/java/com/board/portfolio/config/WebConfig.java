5a1e2cc7243a765feac399bfed9cd7a93c027deb
전태준 <taejoon.jeon@gooroomee.com>
2019-11-25 11:39:59 +0900

    index.html 보여주는 @Controller 생성(기존 ViewController 등록방식에서 변경)

--- src/main/java/com/board/portfolio/config/WebConfig.java
+++ src/main/java/com/board/portfolio/config/WebConfig.java
@@ -2,14 +2,10 @@ package com.board.portfolio.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
-    }
+
 }
