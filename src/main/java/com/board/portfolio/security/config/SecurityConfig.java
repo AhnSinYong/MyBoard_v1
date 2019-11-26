41d91a9783ab07cfcd70a130c80647062e29a65b
전태준 <taejoon.jeon@gooroomee.com>
2019-11-26 15:31:06 +0900

    security config에 jwtFilter,jwtProvider 등록 및 기본 설정

--- src/main/java/com/board/portfolio/security/config/SecurityConfig.java
+++ src/main/java/com/board/portfolio/security/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.board.portfolio.security.config;
 
+import com.board.portfolio.security.filter.FilterSkipMatcher;
+import com.board.portfolio.security.filter.JwtFilter;
+import com.board.portfolio.security.handler.JwtFilterFailureHandler;
+import com.board.portfolio.security.handler.JwtFilterSuccessHandler;
+import com.board.portfolio.security.provider.JwtProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +14,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -15,18 +25,48 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+    @Autowired
+    JwtProvider jwtProvider;
+    @Autowired
+    JwtFilterSuccessHandler jwtFilterSuccessHandler;
+    @Autowired
+    JwtFilterFailureHandler jwtFilterFailureHandler;
+    //filter 생성 및 Manager에 등록
+    @Bean
+    public JwtFilter jwtFilter() throws Exception {
+        FilterSkipMatcher filterSkipMatcher = new FilterSkipMatcher(Arrays.asList("/api/account/signIn"), "/api/**");
+        JwtFilter jwtFilter = new JwtFilter(filterSkipMatcher, jwtFilterSuccessHandler, jwtFilterFailureHandler);
+        jwtFilter.setAuthenticationManager(super.authenticationManagerBean());
+        return jwtFilter;
+    }
+
+    //Manager 등록
     @Bean
     public AuthenticationManager getAuthenticationManager() throws Exception{
         return super.authenticationManagerBean();
     }
 
+    //Provider 등록
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth
+                .authenticationProvider(this.jwtProvider);
     }
 
+    //Filter 등록
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .csrf().disable();
+
+        http
+                .headers().frameOptions().disable();
+
+        http
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
