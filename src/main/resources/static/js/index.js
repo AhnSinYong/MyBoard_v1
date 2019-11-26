b3ea0d8ae2bc2642170ddf69d399f45bf3f04e21
전태준 <taejoon.jeon@gooroomee.com>
2019-11-26 11:08:26 +0900

    index.js security 로직 구현을 위한 간단 new Vue 생성

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,22 @@
+import signUp from"./component/signUp.js"
+
+new Vue({
+    el : '#app',
+    components:{
+        signUp
+    },
+    data(){
+        return {
+
+        }
+    },
+    async created(){
+
+    },
+
+    methods:{
+        showSignUpView(){
+
+        }
+    }
+})
