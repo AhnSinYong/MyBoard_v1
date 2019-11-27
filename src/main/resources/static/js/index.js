5e918d1a87c08d584824e49bbed20d1c98f65ecf
전태준 <taejoon.jeon@gooroomee.com>
2019-11-27 14:47:54 +0900

    index.js에 sign-in 컴포넌트 추가

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,16 +1,21 @@
-import shareObject from "./component/shareObject.js"
+import shareObject from "./component/shareObject/shareObject.js"
 
+import signIn from "./component/signIn.js"
 import signUp from"./component/signUp.js"
 
 new Vue({
     el : '#app',
     components:{
+        signIn,
         signUp
     },
     data(){
         return {
             coverViewState:shareObject.coverView.state,
             coverViewMethod : shareObject.coverView.method,
+
+            loginInfo : shareObject.login.info,
+            loginMethod: shareObject.login.method,
         }
     },
     async created(){
