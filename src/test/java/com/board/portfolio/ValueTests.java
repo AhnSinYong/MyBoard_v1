c191571e49ae3b3cbb05aaa0b2146c126dbba732
Jeon Tae Joon <etea583@naver.com>
2019-12-01 12:43:06 +0900

    properties 테스트

--- src/test/java/com/board/portfolio/ValueTests.java
+++ src/test/java/com/board/portfolio/ValueTests.java
@@ -2,16 +2,26 @@ package com.board.portfolio;
 
 import com.board.portfolio.domain.entity.AccountRole;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:properties/custom.properties")
 class ValueTests {
 
+    @Value("${testaaa.name}")
+    String name;
     @Test
     void enumValueTest(){
         System.out.println(AccountRole.ADMIN.getRoleName());
         System.out.println(AccountRole.MEMBER.getRoleName());
 
         System.out.println(AccountRole.valueOf("ADMIN").getRoleName());
+    }
+
+    @Test
+    void printProperties(){
+        System.out.println(name);
     }
 }
