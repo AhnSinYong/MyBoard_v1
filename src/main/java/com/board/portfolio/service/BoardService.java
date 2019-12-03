21302ed636b107e65b0c9af82a6b2f4b5275ec31
전태준 <taejoon.jeon@gooroomee.com>
2019-12-03 18:27:34 +0900

    게시판 개발을 위한 임시 로직 작성(클라이언트~서버)

---
+++ src/main/java/com/board/portfolio/service/BoardService.java
@@ -0,0 +1,27 @@
+package com.board.portfolio.service;
+
+import com.board.portfolio.domain.entity.Board;
+import com.board.portfolio.paging.BoardPagination;
+import com.board.portfolio.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+
+    private BoardRepository boardRepository;
+    private BoardPagination boardPagination;
+
+    @Autowired
+    public BoardService(BoardRepository boardRepository,
+                        BoardPagination boardPagination){
+        this.boardRepository = boardRepository;
+        this.boardPagination = boardPagination;
+    }
+
+    public List<Board> getPaginBoardList(int page){
+        return null;
+    }
+}
