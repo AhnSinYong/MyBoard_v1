55da7ff94b2ec86bc77015da17308f349c5dcb66
전태준 <taejoon.jeon@gooroomee.com>
2019-12-03 10:42:26 +0900

    이메일 인증을 관리를 위한 @Transactional 추가(new Thread)

--- src/main/java/com/board/portfolio/repository/AccountRepository.java
+++ src/main/java/com/board/portfolio/repository/AccountRepository.java
@@ -2,6 +2,7 @@ package com.board.portfolio.repository;
 
 import com.board.portfolio.domain.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,4 +20,7 @@ public interface AccountRepository extends JpaRepository<Account,String> {
     boolean existsByEmailAndAuthKeyAndIsAuth(String email, String authKey, boolean isAuth);
 
     List<Account> findAllByIsAuthOrderBySignUpDateAsc(boolean isAuth);
+
+    @Transactional
+    void deleteByEmailAndIsAuth(String email, boolean isAuth);
 }
