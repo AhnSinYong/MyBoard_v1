310c6a6a89f028725a42f2cc5f456e55288a938d
전태준 <taejoon.jeon@gooroomee.com>
2019-12-06 15:38:23 +0900

    writePost에 대한 컨트롤러 경로 추가

--- src/main/java/com/board/portfolio/controller/BoardController.java
+++ src/main/java/com/board/portfolio/controller/BoardController.java
@@ -1,9 +1,14 @@
 package com.board.portfolio.controller;
 
+import com.board.portfolio.domain.dto.BoardDTO;
 import com.board.portfolio.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,5 +23,10 @@ public class BoardController {
     @GetMapping("/board/{page}")
     public ResponseEntity getBoardList(@PathVariable int page){
         return ResponseEntity.ok(boardService.getPaginBoardList(page));
+    }
+    @PostMapping("/board")
+    public ResponseEntity writePost(@ModelAttribute BoardDTO.Write dto,
+                                    @ModelAttribute("fileList") List<MultipartFile> fileList){
+        return ResponseEntity.ok(Result.SUCCESS);
     }
 }
