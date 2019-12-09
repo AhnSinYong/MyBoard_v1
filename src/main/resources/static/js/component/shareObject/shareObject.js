8a80b1fe22e180f7b430e6cb5cfbde4cf274d138
전태준 <taejoon.jeon@gooroomee.com>
2019-12-09 13:52:14 +0900

    shareObject에 갱신 기능을 위한 refreshManager 추가

--- src/main/resources/static/js/component/shareObject/shareObject.js
+++ src/main/resources/static/js/component/shareObject/shareObject.js
@@ -1,9 +1,10 @@
 import login from "./share.login.js"
 import coverView from "./share.coverView.js"
 import input from "./share.input.js"
-
+import refreshManager from "./share.refreshManager.js"
 export default {
     login: login,
     coverView: coverView,
-    input: input
+    input: input,
+    refreshManager : refreshManager
 }
