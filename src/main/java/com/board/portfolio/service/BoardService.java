95cc8153fe44774502659ac334be1e7f868f791c
전태준 <taejoon.jeon@gooroomee.com>
2019-12-10 16:45:54 +0900

    BoardService에 readPost() 추가

--- src/main/java/com/board/portfolio/service/BoardService.java
+++ src/main/java/com/board/portfolio/service/BoardService.java
@@ -1,13 +1,12 @@
 package com.board.portfolio.service;
 
 import com.board.portfolio.domain.dto.BoardDTO;
-import com.board.portfolio.domain.entity.Account;
-import com.board.portfolio.domain.entity.Board;
-import com.board.portfolio.domain.entity.FileAttachment;
+import com.board.portfolio.domain.entity.*;
 import com.board.portfolio.exception.FailSaveFileException;
+import com.board.portfolio.exception.NotFoundPostException;
 import com.board.portfolio.paging.BoardPagination;
 import com.board.portfolio.paging.PageDTO;
-import com.board.portfolio.repository.BoardRepository;
+import com.board.portfolio.repository.BoardDetailRepository;
 import com.board.portfolio.repository.FileAttachmentRepository;
 import com.board.portfolio.security.account.AccountSecurityDTO;
 import org.modelmapper.ModelMapper;
@@ -20,24 +19,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class BoardService {
 
-    private BoardRepository boardRepository;
+    private BoardDetailRepository boardDetailRepository;
     private FileAttachmentRepository fileAttachmentRepository;
     private BoardPagination boardPagination;
     private ModelMapper modelMapper;
 
     @Autowired
-    public BoardService(BoardRepository boardRepository,
+    public BoardService(BoardDetailRepository boardDetailRepository,
                         FileAttachmentRepository fileAttachmentRepository,
                         BoardPagination boardPagination,
                         ModelMapper modelMapper){
-        this.boardRepository = boardRepository;
+        this.boardDetailRepository = boardDetailRepository;
         this.fileAttachmentRepository = fileAttachmentRepository;
         this.boardPagination = boardPagination;
         this.modelMapper = modelMapper;
@@ -50,10 +47,10 @@ public class BoardService {
     @Transactional
     public void writePost(BoardDTO.Write boardDTO, AccountSecurityDTO accountDTO) {
 
-        Board board = modelMapper.map(boardDTO, Board.class);
+        BoardDetail board = modelMapper.map(boardDTO, BoardDetail.class);
         Account account = modelMapper.map(accountDTO, Account.class);
         board.setAccount(account);
-        board = boardRepository.save(board);
+        board = boardDetailRepository.save(board);
         try {
             if(!boardDTO.isNullFileList()){
                 saveFileAttachment(board, boardDTO.getFileList(), account);
@@ -64,7 +61,7 @@ public class BoardService {
         }
 
     }
-    private void saveFileAttachment(Board board, List<MultipartFile> multipartFileList, Account account) throws IOException{
+    private void saveFileAttachment(BoardDetail board, List<MultipartFile> multipartFileList, Account account) throws IOException{
         for(MultipartFile file : multipartFileList){
             String originName = file.getOriginalFilename();
             String saveName = UUID.randomUUID().toString();
@@ -86,4 +83,16 @@ public class BoardService {
         }
     }
 
+    @Transactional
+    public Map readPost(long boardId) {
+        BoardDetail boardDetail = boardDetailRepository.findById(boardId).orElseThrow(()->new NotFoundPostException());
+        List<FileAttachment> fileAttachmentList = boardDetail.getFileAttachmentList();
+        List<Comment> commentList = boardDetail.getCommentList();
+
+        Map data = new HashMap<String,Object>();
+        data.put("post",boardDetail);
+        data.put("fileList", fileAttachmentList);
+        data.put("commentList",commentList);
+        return data;
+    }
 }
