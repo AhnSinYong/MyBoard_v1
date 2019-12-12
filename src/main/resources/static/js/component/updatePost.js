d46a35adfee266812a32e3b0b3df332bfefdce94
전태준 <taejoon.jeon@gooroomee.com>
2019-12-12 18:30:10 +0900

    updatePost.js 핵심 기능 완성

--- src/main/resources/static/js/component/updatePost.js
+++ src/main/resources/static/js/component/updatePost.js
@@ -10,16 +10,20 @@ export default Vue.component('update-post',{
                     <div><input v-model="post.title"type="text"></div>
                     <div><textarea v-model="post.content"></textarea></div>
                     <div>
-                        <div v-for="(file,index) in fileList">
+                        <div v-for="(file,index) in post.fileList">
                             <span>{{file.originName}}</span>
-                            <input type="button" value="delete" @click="deleteFile(index)">
+                            <input type="button" value="x" @click="deleteFile(index)">
+                        </div>                        
+                        <div v-for="(file,index) in input.fileList">
+                            <span> name : {{file.name}}, size : {{file.size}}</span>
+                            <input type="button" value="x" @click="removeFile(index)">
                         </div>
                         <div>
-                            <input type="file"><span>upload file</span>
+                            <input type="file" ref="fileInput" @change="addFile()" ><span>upload file</span>
                         </div>
                     </div>
                     <div>
-                        <input type="button" value="complete">
+                        <input type="button" value="complete" @click="updatePost(post, input.fileList)">
                         <input type="button" value="cancel" @click="returnPost()">
                     </div>
                 </div>                
@@ -34,16 +38,15 @@ export default Vue.component('update-post',{
             inputMethod : shareObject.input.method,
             loginInfo : shareObject.login.info,
             loginMethod : shareObject.login.method,
-            input:{
-
-            },
 
             post:{
                 title:'',
                 content:'',
             },
-            fileList:[],
 
+            input:{
+                fileList:[]
+            },
         }
     },
     async created(){
@@ -51,23 +54,41 @@ export default Vue.component('update-post',{
     watch:{
         board(board, oldVal){
             this.post = board.post;
-            this.fileList = board.fileList;
+            this.post.fileList = board.fileList;
         }
     },
     methods:{
-        updatePost(boardId){
-            // axios.get('/api/board/'+boardId)
-            //     .then(this.successUpdatePost)
-            //     .catch(this.fail)
+        updatePost(post,fileList){
+            const formData = new FormData();
+            formData.append('title',post.title);
+            formData.append('content', post.content);
+
+            for(let i=0; i<post.fileList.length; i++){
+                const file = post.fileList[i];
+                const email = file.account.email;
+                const boardId = file.board.boardId;
+                const fileId = file.fileId;
+                formData.append('existFileInfoList['+i+'].email', email);
+                formData.append('existFileInfoList['+i+'].boardId', boardId);
+                formData.append('existFileInfoList['+i+'].fileId', fileId);
+            }
+
+            for(let i=0; i<fileList.length; i++){
+                formData.append('inputFileList['+i+']', fileList[i]);
+            }
+
+            axios.put('/api/board/'+post.boardId,formData,{
+                headers : {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+                .then(this.successUpdatePost)
+                .catch(this.fail)
         },
         successUpdatePost(res){
             const data = res.data;
 
-            this.post = data.post;
-            this.post.isLiked = data.isLikedPost;
-            this.commentList = data.commentList;
-            this.fileList = data.fileList;
-
+            this.coverViewMethod.showPostView();
             this.inputMethod.resetInput(this.input);
             shareObject.refreshManager.refresh();
         },
@@ -76,7 +97,15 @@ export default Vue.component('update-post',{
             alert(err.data);
         },
         deleteFile(index){
-            this.fileList.splice(index,1);
+            this.post.fileList.splice(index,1);
+            this.$forceUpdate();
+        },
+        removeFile(index){
+            this.input.fileList.splice(index,1);
+        },
+        addFile(){
+            this.input.fileList.push(this.$refs.fileInput.files[0]);
+            this.$refs.fileInput.value='';
         },
         returnPost(){
             this.coverViewMethod.showPostView();
