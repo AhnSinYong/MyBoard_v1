27ebb10e7b512f102c9a01942f078f9d3bcf89e1
전태준 <taejoon.jeon@gooroomee.com>
2019-12-13 18:01:04 +0900

    CommentController에 writeComment,modifyComment,deleteComment추가

--- src/main/java/com/board/portfolio/controller/CommentController.java
+++ src/main/java/com/board/portfolio/controller/CommentController.java
@@ -1,10 +1,14 @@
 package com.board.portfolio.controller;
 
+import com.board.portfolio.domain.dto.CommentDTO;
 import com.board.portfolio.security.account.AccountSecurityDTO;
 import com.board.portfolio.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
@@ -20,6 +24,29 @@ public class CommentController {
     public ResponseEntity getCommentList(@PathVariable Long boardId,
                                          @ModelAttribute("accountDTO") AccountSecurityDTO accountDTO){
         return ResponseEntity.ok(commentService.getCommentList(boardId));
+    }
+
+    @PreAuthorize("hasRole('ROLE_MEMBER')")
+    @PostMapping("/comment/{boardId}")
+    public ResponseEntity writeComment(@PathVariable Long boardId,
+                                       @RequestBody @Valid CommentDTO.Write dto,
+                                       @ModelAttribute("accountDTO") AccountSecurityDTO accountDTO){
+        return ResponseEntity.ok(commentService.writeComment(boardId,dto,accountDTO));
+    }
+    @PreAuthorize("hasRole('ROLE_MEMBER')")
+    @PutMapping("/comment/{commentId}")
+    public ResponseEntity modifyComment(@PathVariable Long commentId,
+                                        @RequestBody @Valid CommentDTO.Modify dto,
+                                        @ModelAttribute("accountDTO") AccountSecurityDTO accountDTO){
+
+        return ResponseEntity.ok(commentService.modifyComment(commentId,dto,accountDTO));
+    }
+    @PreAuthorize("hasRole('ROLE_MEMBER')")
+    @DeleteMapping("/comment/{commentId}")
+    public ResponseEntity deleteComment(@PathVariable Long commentId,
+                                        @ModelAttribute("accountDTO") AccountSecurityDTO accountDTO){
+
+        return ResponseEntity.ok(commentService.deleteComment(commentId, accountDTO));
     }
 
 
