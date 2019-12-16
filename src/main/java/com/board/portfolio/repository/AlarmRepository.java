20e51097e35544d3a6e8d3f45d77f5c474398dbf
전태준 <taejoon.jeon@gooroomee.com>
2019-12-16 18:26:43 +0900

    AlarmRepository에 쿼리 메소드 추가

--- src/main/java/com/board/portfolio/repository/AlarmRepository.java
+++ src/main/java/com/board/portfolio/repository/AlarmRepository.java
@@ -1,8 +1,13 @@
 package com.board.portfolio.repository;
 
+import com.board.portfolio.domain.entity.Account;
 import com.board.portfolio.domain.entity.Alarm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Map;
+
 public interface AlarmRepository extends JpaRepository<Alarm,String> {
 
+    List<Alarm> findAllByTargetAccountOrderByRecieveDateDesc(Account account);
 }
