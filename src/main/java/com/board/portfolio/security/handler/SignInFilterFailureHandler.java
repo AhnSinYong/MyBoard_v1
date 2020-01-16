683d208c9f4b4d9526de7c1ab8c650cc8fbc81cd
Jeon Tae Joon <etea583@naver.com>
2020-01-16 21:36:00 +0900

    SignInFilterFailureHandler에 예외응답 구현

--- src/main/java/com/board/portfolio/security/handler/SignInFilterFailureHandler.java
+++ src/main/java/com/board/portfolio/security/handler/SignInFilterFailureHandler.java
@@ -1,5 +1,11 @@
 package com.board.portfolio.security.handler;
 
+import com.board.portfolio.exception.ApiError;
+import com.board.portfolio.exception.GlobalErrorContent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -8,11 +14,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Component
+@RequiredArgsConstructor
 public class SignInFilterFailureHandler implements AuthenticationFailureHandler {
+    private final MessageSource validMessageSource;
+    private final ResponseHandler responseHandler;
     @Override
     public void onAuthenticationFailure(HttpServletRequest req, HttpServletResponse res, AuthenticationException e) throws IOException, ServletException {
-
+        String message = validMessageSource.getMessage(e.getMessage(), null, LocaleContextHolder.getLocale());
+        responseHandler.response400(res, new ApiError(HttpStatus.BAD_REQUEST, Arrays.asList(new GlobalErrorContent(message))));
     }
+
+
 }
