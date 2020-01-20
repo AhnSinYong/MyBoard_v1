6425ca209fce455d29112eefb32d18cabae2890a
Jeon Tae Joon <etea583@naver.com>
2020-01-20 12:30:35 +0900

    AccountDetailsService @RequiredArgsConstructor 방식으로 변경

--- src/main/java/com/board/portfolio/security/account/AccountDetailsService.java
+++ src/main/java/com/board/portfolio/security/account/AccountDetailsService.java
@@ -1,19 +1,18 @@
 package com.board.portfolio.security.account;
 
 import com.board.portfolio.domain.entity.Account;
-import com.board.portfolio.exception.custom.NotFoundEmailException;
 import com.board.portfolio.repository.AccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.board.portfolio.security.exception.NotFoundEmailException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class AccountDetailsService implements UserDetailsService {
-    @Autowired
-    AccountRepository accountRepository;
-
+    private final AccountRepository accountRepository;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Account account = accountRepository.findById(email).orElseThrow(NotFoundEmailException::new);
