42c2a05328b5f5715d57fefc872a76c23e34274f
Jeon Tae Joon <etea583@naver.com>
2020-01-22 23:02:58 +0900

    ResponseHandler에 getLocale() 추가

--- src/main/java/com/board/portfolio/security/handler/ResponseHandler.java
+++ src/main/java/com/board/portfolio/security/handler/ResponseHandler.java
@@ -5,10 +5,13 @@ import com.board.portfolio.exception.GlobalErrorContent;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Locale;
+import java.util.Optional;
 
 import static com.board.portfolio.util.StaticUtils.objectMapper;
 
@@ -27,5 +30,10 @@ public class ResponseHandler {
     }
     public void jwtResponse400(HttpServletResponse res, String message) throws IOException {
         response400(res,new ApiError(HttpStatus.BAD_REQUEST, Arrays.asList(new GlobalErrorContent(message))));
+    }
+    public static Locale getLocale(HttpServletRequest req){
+        Optional<String> langOp = Optional.ofNullable(req.getParameter("lang"));
+        String lang = langOp.orElse("ko");
+        return new Locale(lang);
     }
 }
