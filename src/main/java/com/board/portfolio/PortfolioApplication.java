608675d37b564e2a4952a114a914eb16bd9a3dca
Jeon Tae Joon <etea583@naver.com>
2020-01-23 20:57:25 +0900

    @EnableJpaAuditing 추가

--- src/main/java/com/board/portfolio/PortfolioApplication.java
+++ src/main/java/com/board/portfolio/PortfolioApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @PropertySources({
         @PropertySource("classpath:properties/private/database-config.properties"),
         @PropertySource("classpath:properties/private/mail-config.properties"),
