edc5d2c3b22989984bff7b74cb919fa04bf20953
Jeon Tae Joon <etea583@naver.com>
2020-01-25 15:34:16 +0900

    application.java 에 @EnableCaching 추가

--- src/main/java/com/board/portfolio/PortfolioApplication.java
+++ src/main/java/com/board/portfolio/PortfolioApplication.java
@@ -2,12 +2,14 @@ package com.board.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 @PropertySources({
         @PropertySource("classpath:properties/private/database-config.properties"),
         @PropertySource("classpath:properties/private/mail-config.properties"),
