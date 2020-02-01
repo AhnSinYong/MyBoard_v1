a3cde9aa1db8c024e2c9c24c921745bbeca912d6
Jeon Tae Joon <etea583@naver.com>
2020-02-01 21:49:17 +0900

    BoardPagination이 StoredBoardRepository를 사용하도록 변경

--- src/main/java/com/board/portfolio/paging/BoardPagination.java
+++ src/main/java/com/board/portfolio/paging/BoardPagination.java
@@ -2,6 +2,7 @@ package com.board.portfolio.paging;
 
 import com.board.portfolio.domain.entity.Board;
 import com.board.portfolio.repository.BoardRepository;
+import com.board.portfolio.store.repository.StoredBoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -9,13 +10,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class BoardPagination extends Pagination {
 
+    private StoredBoardRepository storedBoardRepository;
     private BoardRepository boardRepository;
     @Autowired
     public BoardPagination(@Value("${board.pageSize}") int pageSize,
                            @Value("${board.rangeSize}") int rangeSize,
-                           BoardRepository boardRepository){
+                           BoardRepository boardRepository,
+                           StoredBoardRepository storedBoardRepository){
         super(pageSize, rangeSize);
         this.boardRepository = boardRepository;
+        this.storedBoardRepository = storedBoardRepository;
     }
 
     @Override
@@ -24,13 +28,13 @@ public class BoardPagination extends Pagination {
         long total = boardRepository.count();
         long start = (page-1) * super.pageSize+1;
         long totalPage = (long) Math.ceil((double)total/(double)pageSize);
-        long startPage = (page/rangeSize)*rangeSize+1;
+        long startPage = ((page-1)/rangeSize)*rangeSize+1;
         long endPage = (startPage+rangeSize-1)>totalPage?totalPage:(startPage+rangeSize-1);
         long prevPage = (page-1)>0?(page-1):-1;
         long nextPage = (page+1)<=totalPage?(page+1):-1;
 
 
-        return new PageDTO(boardRepository.getBoardList(start, super.pageSize),
+        return new PageDTO(storedBoardRepository.getBoardList(page, start, super.pageSize),
                 page,
                 startPage,
                 endPage,
