cb7e35c7c5eec5658f33c8d866d3de09db4b6af1
Jeon Tae Joon <etea583@naver.com>
2020-02-01 21:47:18 +0900

    BoardCore에 service 메소드들 StoreRepository 고려한 로직으로 오버로딩

--- src/main/java/com/board/portfolio/domain/entity/BoardCore.java
+++ src/main/java/com/board/portfolio/domain/entity/BoardCore.java
@@ -1,6 +1,9 @@
 package com.board.portfolio.domain.entity;
 
+import com.board.portfolio.store.repository.StoredBoardRepository;
+import com.board.portfolio.store.repository.StoredEntityIdentifier;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,10 +16,11 @@ import java.util.Optional;
 
 @MappedSuperclass
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BoardCore extends EntityDefaultValues{
+public abstract class BoardCore extends EntityDefaultValues implements StoredEntityIdentifier<Long> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -52,11 +56,28 @@ public abstract class BoardCore extends EntityDefaultValues{
     public void increaseLike(){
         this.like++;
     }
+    public void increaseLike(StoredBoardRepository storedBoardRepository){
+        this.like++;
+        storedBoardRepository.increaseLike(this.boardId);
+    }
     public void decreaseLike(){
         this.like--;
     }
+    public void decreaseLike(StoredBoardRepository storedBoardRepository){
+        this.like--;
+        storedBoardRepository.decreaseLike(this.boardId);
+    }
     public void increaseView(){
         this.view++;
+    }
+    public void increaseView(StoredBoardRepository storedBoardRepository){
+        this.view++;
+        storedBoardRepository.increaseView(this.boardId);
+    }
+
+    public void updatePost(String title, LocalDateTime upDate){
+        this.title = title;
+        this.upDate = upDate;
     }
 
     @Override
