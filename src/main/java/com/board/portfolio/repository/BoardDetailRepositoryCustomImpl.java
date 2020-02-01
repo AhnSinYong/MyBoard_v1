939ca0b510c2e12e65904a0293bbbb84da7ae0c8
Jeon Tae Joon <etea583@naver.com>
2020-02-01 21:48:48 +0900

    BoardDetailRepositoryCustomImpl에 getBoardList() 구현

--- src/main/java/com/board/portfolio/repository/BoardDetailRepositoryCustomImpl.java
+++ src/main/java/com/board/portfolio/repository/BoardDetailRepositoryCustomImpl.java
@@ -1,9 +1,24 @@
 package com.board.portfolio.repository;
 
+import com.board.portfolio.domain.entity.Board;
+import com.board.portfolio.domain.entity.BoardDetail;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.board.portfolio.domain.entity.QBoardDetail.boardDetail;
 
 @RequiredArgsConstructor
 public class BoardDetailRepositoryCustomImpl implements BoardDetailRepositoryCustom {
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<BoardDetail> getBoardList(int size) {
+        return queryFactory
+                .selectFrom(boardDetail)
+                .orderBy(boardDetail.regDate.desc())
+                .limit(size)
+                .fetch();
+    }
 }
