834555ba6ae6ee29b652ee317d98751ac5f12a29
Jeon Tae Joon <etea583@naver.com>
2020-02-01 21:46:23 +0900

    Board @Entity에 생성자 추가 및 StoredEntityIdentifier 인터페이스 구현

--- src/main/java/com/board/portfolio/domain/entity/Board.java
+++ src/main/java/com/board/portfolio/domain/entity/Board.java
@@ -1,14 +1,13 @@
 package com.board.portfolio.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Entity
 @Table(name = "TB_BOARD")
@@ -32,5 +31,14 @@ public class Board extends BoardCore {
 
     public Board(Long boardId) {
         super(boardId);
+    }
+    public Board(Long boardId, String title , Integer like, Integer view,
+                 LocalDateTime regDate, LocalDateTime update, Account account){
+        super(boardId,title,like,view,regDate,update,account);
+    }
+
+    @Override
+    public Long getId() {
+        return getBoardId();
     }
 }
