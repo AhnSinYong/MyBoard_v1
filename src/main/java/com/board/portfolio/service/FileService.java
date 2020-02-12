54b30b1eaa505d0684a5aaa74a08ab01cda95f78
Jeon Tae Joon <etea583@naver.com>
2020-02-12 17:42:09 +0900

    FileService에 update, delete 로직 추가

--- src/main/java/com/board/portfolio/service/FileService.java
+++ src/main/java/com/board/portfolio/service/FileService.java
@@ -11,14 +11,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.bind.DatatypeConverter;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -28,10 +26,7 @@ import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Component
@@ -43,12 +38,12 @@ public class FileService {
     private String ATTACHMENT_FILE_PATH;
     @Value("${file.save-path.img}")
     private String IMG_FILE_PATH;
+    @Value("${file.img.req-path}")
+    private String REQ_PATH;
 
     public String Base64ToImg(String base64,String extension){
         String name = UUID.randomUUID().toString();
-
-        byte[] imageBytes = DatatypeConverter.parseBase64Binary(base64);
-//        byte[] imageBytes = Base64.getDecoder().decode(base64.trim());
+        byte[] imageBytes = Base64.getDecoder().decode(base64.trim());
 
         try {
             BufferedImage bufImg = ImageIO.read(new ByteArrayInputStream(imageBytes));
@@ -57,7 +52,52 @@ public class FileService {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return IMG_FILE_PATH.split("/resources")[1]+name+"."+extension;
+        return REQ_PATH+name+"."+extension;
+    }
+    public void updateStoreImgFolder(String content, String dtoContent){
+        List<String> transContents =  transContentToImgSrc(content);
+        List<String> transContentDtos = transContentToImgSrc(dtoContent);
+
+        for(String transContent : transContents){
+            boolean isContain = transContentDtos.contains(transContent);
+            if(!isContain){
+                String[] parts = transContent.split("/");
+                String fileName = parts[parts.length-1];
+                deleteStoreImgFilePhysic(fileName);
+            }
+        }
+
+    }
+    public List<String> transContentToImgSrc(String content){
+        String[] parts = content.split("<img .*?src\\s*=\\s*\"");
+        List<String> result = new ArrayList();
+        for(String part : parts){
+            if(part.indexOf(REQ_PATH)==0){
+                result.add(part.split("\".*>")[0]);
+            }
+        }
+        return result;
+    }
+    public String transImgSrc(String content){
+        String[] parts = content.split("data:image/");
+
+        for(String part : parts){
+            if(hasImgTag(part)){
+                String[] data = part.split(";");
+                String extension = data[0];
+                String filePath = Base64ToImg(data[1].split(",")[1].split("\"")[0],extension);
+                content = content.replaceFirst("data:image/"+extension+";base64.*?\"",filePath+"\"");
+            }
+        }
+        return content;
+    }
+
+    private boolean hasImgTag(String content){
+        boolean state = false;
+        if(content.split(";").length>1){
+            state = content.split(";")[1].indexOf("base64")==0;
+        }
+        return state;
     }
 
     public void saveFileAttachment(BoardDetail board, List<MultipartFile> multipartFileList, Account account) throws IOException {
@@ -120,8 +160,15 @@ public class FileService {
         }
 
     }
+    public void deleteStoreImgFilePhysic(String fileName){
 
-    public void deleteFilePhysic(List<FileAttachment> fileAttachmentList){
+        File file = new File(IMG_FILE_PATH+fileName);
+        if(file.exists())
+            file.delete();
+
+    }
+
+    public void deleteStoreAttachFilePhysic(List<FileAttachment> fileAttachmentList){
         for(FileAttachment fileAttachment : fileAttachmentList){
             File file = new File(ATTACHMENT_FILE_PATH+fileAttachment.getSaveName());
             if(file.exists())
@@ -136,7 +183,7 @@ public class FileService {
         }
         for(FileAttachment file : fileAttachmentList){
             if(!dto.isExistFileId(file.getFileId())){
-                deleteFilePhysic(Arrays.asList(file));
+                deleteStoreAttachFilePhysic(Arrays.asList(file));
                 fileAttachmentRepository.delete(file);
             }
         }
