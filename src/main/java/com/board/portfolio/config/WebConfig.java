3e9919167ab38bdace4f0c1133a1c77efd527e3f
Jeon Tae Joon <etea583@naver.com>
2020-02-26 12:55:43 +0900

    첨부파일, 에디터 이미지가 application 바깥에 저장되도록 수정

--- src/main/java/com/board/portfolio/config/WebConfig.java
+++ src/main/java/com/board/portfolio/config/WebConfig.java
@@ -1,17 +1,22 @@
 package com.board.portfolio.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${file.resource-path.img}")
+    private String imgPath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:static/");
         registry.addResourceHandler("/store/img/**")
-                .addResourceLocations("classpath:store/img/")
-                .setCachePeriod(0);
+                .addResourceLocations(imgPath)
+                .setCachePeriod(300);
+
     }
 }
